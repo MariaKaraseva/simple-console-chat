67bdf6318300e2b4a46c07092d5dd79a64ab5cd4
Мария Карасева <mariaevg07@mail.ru>
2021-06-20 19:49:25 +0500

    final version

---
+++ src/com/chat/client/Client.java
@@ -0,0 +1,115 @@
+package com.chat.client;
+
+import java.net.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Client {
+
+    public static String ipAddr = "localhost";
+    public static int port = 8080;
+
+    public static void main(String[] args) {
+        new ClientInstance(ipAddr, port);
+    }
+}
+
+class ClientInstance {
+
+    private Socket socket;
+    private BufferedReader in; // поток чтения из сокета
+    private BufferedWriter out; // поток чтения в сокет
+    private BufferedReader inputUser; // поток чтения с консоли
+    private String addr; // ip адрес клиента
+    private int port; // порт соединения
+    private String nickname; // имя клиента
+
+    public ClientInstance(String addr, int port) {
+        this.addr = addr;
+        this.port = port;
+        try {
+            this.socket = new Socket(addr, port);
+        } catch (IOException e) {
+            System.err.println("Сервер не запущен, попробуйте позже");
+        }
+        try {
+            inputUser = new BufferedReader(new InputStreamReader(System.in));
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.pressNickname();
+            new ReadMsg().start();
+            new WriteMsg().start();
+        } catch (IOException e) {
+            ClientInstance.this.downService();
+        }
+    }
+
+    private void pressNickname() {
+        System.out.print("Введите свой ник: ");
+        try {
+            nickname = inputUser.readLine();
+            out.write("Добро пожаловать, " + nickname + "\n");
+            out.flush();
+        } catch (IOException ignored) {
+        }
+    }
+
+    private void downService() {
+        try {
+            if (!socket.isClosed()) {
+                socket.close();
+                in.close();
+                out.close();
+            }
+        } catch (IOException ignored) {}
+    }
+
+    private class ReadMsg extends Thread {
+        @Override
+        public void run() {
+
+            String str;
+            try {
+                while (true) {
+                    str = in.readLine();
+                    if (str.equals("stop")) {
+                        ClientInstance.this.downService();
+                        break;
+                    }
+                    System.out.println(str);
+                }
+            } catch (IOException e) {
+                ClientInstance.this.downService();
+            }
+        }
+    }
+
+    public class WriteMsg extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                String userWord;
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yy HH:mm:ss");
+                    String time = dateFormat.format(new Date());
+                    userWord = inputUser.readLine();
+                    if (userWord.equals("stop")) {
+                        out.write("stop");
+                        out.flush();
+                        ClientInstance.this.downService();
+                        break;
+                    } else {
+                        out.write("(" + time + ", " + nickname + "): " + userWord + "\n");
+                    }
+                    out.flush(); // чистим
+                } catch (IOException e) {
+                    ClientInstance.this.downService();
+
+                }
+
+            }
+        }
+    }
+}
